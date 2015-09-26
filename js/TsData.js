39a5c2bf133ee26ba206c57f8441ff3d638ef76f
Lars Boegild Thomsen <lth@netcompartner.com>
2015-09-26 16:15:38 +0800

    Fixed

--- js/TsData.js
+++ js/TsData.js
@@ -158,7 +158,7 @@ TsData.prototype.getDataTable = function(callback) {
     if (this.getRound()) requestUrl += '?round=' + this.getRound();
     if (this.getTimescale()) requestUrl += '?timescale=' + this.getTimescale();
     if (this.getSum()) requestUrl += '?sum=' + this.getSum();
-    if (this.getAverage()) requestUrl += '?averate=' + this.getAverage();
+    if (this.getAverage()) requestUrl += '?average=' + this.getAverage();
     if (this.getMedian()) requestUrl += '?median=' + this.getMedian();
 
     console.log("Request URL: " + requestUrl);
@@ -166,98 +166,98 @@ TsData.prototype.getDataTable = function(callback) {
     // get the data from thingspeak
     $.getJSON(requestUrl, function(data) {
 
-        console.log("Channel id: " + data.channel.id);
-        console.log("Channel name: " + data.channel.name);
-        console.log("Channel description: " + data.channel.description);
+        //console.log("Channel id: " + data.channel.id);
+        //console.log("Channel name: " + data.channel.name);
+        //console.log("Channel description: " + data.channel.description);
 
         if (data.channel.field1) {
-            console.log("Channel field 1: " + data.channel.field1);
+            //console.log("Channel field 1: " + data.channel.field1);
             dataTable.addColumn('number', data.channel.field1);
         }
 
         if (data.channel.field2) {
-            console.log("Channel field 2: " + data.channel.field2);
+            //console.log("Channel field 2: " + data.channel.field2);
             dataTable.addColumn('number', data.channel.field2);
         }
 
         if (data.channel.field3) {
-            console.log("Channel field 3: " + data.channel.field3);
+            //console.log("Channel field 3: " + data.channel.field3);
             dataTable.addColumn('number', data.channel.field3);
         }
 
         if (data.channel.field4) {
-            console.log("Channel field 4: " + data.channel.field4);
+            //console.log("Channel field 4: " + data.channel.field4);
             dataTable.addColumn('number', data.channel.field4);
         }
 
         if (data.channel.field5) {
-            console.log("Channel field 5: " + data.channel.field5);
+            //console.log("Channel field 5: " + data.channel.field5);
             dataTable.addColumn('number', data.channel.field5);
         }
 
         if (data.channel.field6) {
-            console.log("Channel field 6: " + data.channel.field6);
+            //console.log("Channel field 6: " + data.channel.field6);
             dataTable.addColumn('number', data.channel.field6);
         }
 
         if (data.channel.field7) {
-            console.log("Channel field 7: " + data.channel.field7);
+            //console.log("Channel field 7: " + data.channel.field7);
             dataTable.addColumn('number', data.channel.field7);
         }
 
         if (data.channel.field8) {
-            console.log("Channel field 8: " + data.channel.field8);
+            //console.log("Channel field 8: " + data.channel.field8);
             dataTable.addColumn('number', data.channel.field8);
         }
 
-        console.log("Creation date: " + data.channel.created_at);
-        console.log("Creation date: " + data.channel.updated_at);
+        //console.log("Creation date: " + data.channel.created_at);
+        //console.log("Creation date: " + data.channel.updated_at);
 
         $.each( data.feeds, function( key, val ) {
-            console.log("Got Feed Item: " + key);
-            console.log("  Item created at: " + val.created_at);
+            //console.log("Got Feed Item: " + key);
+            //console.log("  Item created at: " + val.created_at);
 
             var row = dataTable.addRow();
 
             dataTable.setCell(row, 0, new Date(val.created_at));
 
             if (data.channel.field1) {
-                console.log("  Item field 1   : " + val.field1);
+                //console.log("  Item field 1   : " + val.field1);
                 dataTable.setCell(row, 1, Number(val.field1));
             }
 
             if (data.channel.field2) {
-                console.log("  Item field 2   : " + val.field2);
+                //console.log("  Item field 2   : " + val.field2);
                 dataTable.setCell(row, 2, Number(val.field2));
             }
 
             if (data.channel.field3) {
-                console.log("  Item field 3   : " + val.field3);
+                //console.log("  Item field 3   : " + val.field3);
                 dataTable.setCell(row, 3, Number(val.field3));
             }
 
             if (data.channel.field4) {
-                console.log("  Item field 4   : " + val.field4);
+                //console.log("  Item field 4   : " + val.field4);
                 dataTable.setCell(row, 4, Number(val.field4));
             }
 
             if (data.channel.field5) {
-                console.log("  Item field 5   : " + val.field5);
+                //console.log("  Item field 5   : " + val.field5);
                 dataTable.setCell(row, 5, Number(val.field5));
             }
 
             if (data.channel.field6) {
-                console.log("  Item field 6   : " + val.field6);
+                //console.log("  Item field 6   : " + val.field6);
                 dataTable.setCell(row, 6, Number(val.field6));
             }
 
             if (data.channel.field7) {
-                console.log("  Item field 7   : " + val.field7);
+                //console.log("  Item field 7   : " + val.field7);
                 dataTable.setCell(row, 7, Number(val.field7));
             }
 
             if (data.channel.field8) {
-                console.log("  Item field 8   : " + val.field8);
+                //console.log("  Item field 8   : " + val.field8);
                 dataTable.setCell(row, 8, Number(val.field8));
             }
 
